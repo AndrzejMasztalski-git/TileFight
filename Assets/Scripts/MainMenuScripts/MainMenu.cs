fc87d08e81088909a69cab3db8023031a8a37b56
andrzejmasztalski <72304329+AndrzejMasztalski-git@users.noreply.github.com>
2023-10-28 11:08:42 +0200

    Initial UI

---
+++ Assets/Scripts/MainMenuScripts/MainMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Canvas mainCanvas;
+    public Canvas settingsCanvas;
+
+    private void Start()
+    {
+        mainCanvas.GetComponent<Canvas>().enabled = true;
+        settingsCanvas.GetComponent<Canvas>().enabled = false;
+    }
+
+    public void SettingsButtonClicked()
+    {
+        mainCanvas.GetComponent<Canvas>().enabled = false;
+        settingsCanvas.GetComponent<Canvas>().enabled = true;
+    }
+
+    public void BackButtonClicked()
+    {
+        mainCanvas.GetComponent<Canvas>().enabled = true;
+        settingsCanvas.GetComponent<Canvas>().enabled = false;
+    }
+
+    public void NewGameButtonClicked()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
+
